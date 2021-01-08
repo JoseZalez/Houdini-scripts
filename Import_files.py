e33d4b6bff79575b1b4bdb5c62a4405a46b5c8b7
Jose Luis Gonzalez <josezalezvfx@gmail.com>
2021-01-08 10:21:21 +0000

    Update Import_files.py

--- Import_files.py
+++ Import_files.py
@@ -1,6 +1,9 @@
 #Get a path and a file type and import all the objects matching in the folder + subdirectories
 #https://www.linkedin.com/in/jose-gonzalezvfx/
 
+#Get a path and a file type and import all the objects matching in the folder + subdirectories
+#https://www.linkedin.com/in/jose-gonzalezvfx/
+
 import os
 
 import sys
@@ -51,6 +54,13 @@ class UI(QDialog):
         fbx_layout.addWidget(QLabel("Import as FBX:"))
         fbx_layout.addWidget(self.fbx)
         fbx_layout.setSpacing(1)
+       
+        #Create a check for abc import
+        abc_layout = QHBoxLayout()
+        self.abc = QCheckBox("", self)
+        abc_layout.addWidget(QLabel("Import as Alembic:"))
+        abc_layout.addWidget(self.abc)
+        abc_layout.setSpacing(1)
 
         #Set a button to start
         self.button = QPushButton('Create')
@@ -60,6 +70,7 @@ class UI(QDialog):
         main_layout.addLayout(extension_layout, stretch=1)
         main_layout.addLayout(subdir_layout, stretch=1)
         main_layout.addLayout(fbx_layout, stretch=1)
+        main_layout.addLayout(abc_layout, stretch=1)
         main_layout.addWidget(self.button)
         self.setLayout(main_layout)
         
@@ -68,11 +79,7 @@ class UI(QDialog):
 
     def main(self):
 
-        if self.fbx.isChecked():
-            self.importfbx()
-
-        else:
-            self.importfiles()
+        self.importfiles()
 
 
     def importfiles(self):
@@ -85,9 +92,9 @@ class UI(QDialog):
         extension = self.extension.text()
 
         #Checks if the user closed the path, if not it closes it
-        if not path.endswith("\\"):
-                path=path + "\\"
-
+        if not path.endswith("/"):
+                path=path + "/"
+                
         files = []
 
         #Check if the user selected a Node, if not returns a message
@@ -108,10 +115,22 @@ class UI(QDialog):
                 
                 #Iterate for every file path and creates a file node with the path loaded
                 for file_path in files:
+                    if self.fbx.isChecked():
+                    
+                        fbx_file = hou.hipFile.importFBX(file_path)
+                        #fbx_file.moveToGoodPosition()
+                      
+                    elif self.abc.isChecked():
+                        
+                        file_node= geo_node[0].createNode("alembic")
+                        file_node.parm("fileName").set(file_path)
+                        file_node.moveToGoodPosition()                        
+                    
+                    else:
                 
-                    file_node= geo_node[0].createNode("file")
-                    file_node.parm("file").set(file_path)
-                    file_node.moveToGoodPosition()
+                        file_node= geo_node[0].createNode("file")
+                        file_node.parm("file").set(file_path)
+                        file_node.moveToGoodPosition()
             else:
                 
                 #The same as before but without iterating inside the subdirectories
@@ -123,54 +142,24 @@ class UI(QDialog):
                         
                         #Checks for files with the same file type as the one the user input
                     if(obj.endswith(extension)):
+                    
+                        if self.fbx.isChecked():
+                        
+                            hou.hipFile.importFBX(obj_path)
+                            #fbx_file.moveToGoodPosition()
+                       
+                        elif self.abc.isChecked():
+                        
+                            file_node= geo_node[0].createNode("alembic")
+                            file_node.parm("fileName").set(file_path)
+                            file_node.moveToGoodPosition()                        
+ 
+                        else:
             
                             file_node= geo_node[0].createNode("file")
                             file_node.parm("file").set(obj_path)
                             file_node.moveToGoodPosition()
         
-    def importfbx(self):
-
-        #Store the path and the extension strings previously input by the user
-        path = self.filepath.text()
-        extension = self.extension.text()
-        #Checks if the user closed the path, if not it closes it
-        if not path.endswith("\\"):
-                path=path + "\\"
-
-        files = []
-
-        if self.subdir.isChecked():
-                    
-                    #Gets all the subdirectories files and creates a path for each
-            for r, d, f in os.walk(path):
-            
-                for file in f:
-
-                    #Checks for files with the same file type as the one the user input
-                    if(file.endswith(extension)):
-                        files.append(os.path.join(r, file))
-            
-            #Iterate for every file path and creates a file node with the path loaded
-            for file_path in files:
-                
-                fbx_file = hou.hipFile.importFBX(file_path)
-                fbx_file.moveToGoodPosition()
-        else:
-            
-            #The same as before but without iterating inside the subdirectories
-            file_list = os.listdir(path)
-        
-            for obj in file_list:
-        
-                obj_path = path+obj
-                print obj_path
-                    
-                #Checks for files with the same file type as the one the user input
-                if(obj.endswith(extension)):
-
-                    hou.hipFile.importFBX(obj_path)
-                    #fbx_file.moveToGoodPosition()
-
 
 #Starts the script window for the user
 app = QApplication.instance()
